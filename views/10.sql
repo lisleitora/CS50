4d473f64b52d0f734f12dce830cc289c0a859a92
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-03 17:02:11 +0000

    Tue, Dec 3, 2024, 5:02 PM +00:00

--- views/10.sql
+++ views/10.sql
@@ -1,3 +1,5 @@
+-- database: views.db
 SELECT id, english_title AS 'ENG title', japanese_title AS 'JP title', artist AS 'creator', average_color AS 'AVG color', brightness, contrast, entropy
 FROM views
-WHERE
+WHERE english_title LIKE '%mount%'
+ORDER BY japanese_title
