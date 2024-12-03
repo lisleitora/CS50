1c5cf81cf0fbaf49481dc13b63ed645302fb0dd9
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-03 11:56:26 +0000

    Tue, Dec 3, 2024, 11:56 AM +00:00

--- views/2.sql
+++ views/2.sql
@@ -1,4 +1,3 @@
 -- database: views.db
 SELECT average_color FROM views
 WHERE english_title LIKE '%river%'
-AND average_color LIKE 
---
+++ views/3.sql
@@ -0,0 +1,3 @@
+
+SELECT average_color FROM views
+WHERE english_title LIKE '%Fuji%'
