0acea7741e924f240c3629e1c68a7ff0d5bff70a
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-04 11:53:12 +0000

    Wed, Dec 4, 2024, 11:53 AM +00:00

--- players/6.sql
+++ players/6.sql
@@ -1,4 +1,4 @@
 -- database: players.db
-SELECT first_name, last_name FROM players
-WHERE 
+SELECT first_name, last_name, debut FROM players
+WHERE birth_city = '' AND birth_state = 'PA'
 ORDER BY first_name, last_name
