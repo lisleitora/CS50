e95777d5bc4c515a35e5f468167365e85669d0c8
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-04 11:05:56 +0000

    Wed, Dec 4, 2024, 11:05 AM +00:00

--- players/1.sql
+++ players/1.sql
@@ -1,5 +1,5 @@
 -- database: players.db
 
-SELECT birth_city, bith_state, birth_country FROM players
+SELECT birth_city, birth_state, birth_country FROM players
 WHERE first_name = 'Jackie'
 AND last_name = 'Robinson'
