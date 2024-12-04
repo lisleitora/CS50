650dcc7f99db4b16f337c30478d29b39d50c68b7
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-04 12:15:46 +0000

    Wed, Dec 4, 2024, 12:15 PM +00:00

--- players/7.sql
+++ players/7.sql
@@ -1,5 +1,4 @@
 -- database: players.db
-SELECT first_name, last_name FROM players
+SELECT COUNT(id) FROM players
 WHERE bats = 'R' AND throws = 'L'
 OR bats = 'L' AND throws = 'R'
-ORDER BY first_name, last_name
