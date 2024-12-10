21bec74cf34415f8f5c61c2fb32c0ab282bcf57b
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-10 11:37:31 +0000

    Tue, Dec 10, 2024, 11:37 AM +00:00

--- packages/trash.sql
+++ packages/trash.sql
@@ -1,3 +1,4 @@
 SELECT type FROM address
 JOIN scans ON (address.id = address_id)
-JOIN packages ON ()
+JOIN packages ON (packages.id = package_id)
+WHERE from_address_id = '900 Somerville Avenue' AND
