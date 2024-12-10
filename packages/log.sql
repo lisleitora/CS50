e3dd79b546fef872b25a91c71204f550589860c5
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-10 17:17:12 +0000

    Tue, Dec 10, 2024, 5:17 PM +00:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,8 +1,10 @@
 
 -- *** The Lost Letter ***
+
 -- to discover the id of the woman who send the letter
 SELECT * FROM addresses
 WHERE address = '900 Somerville Avenue'
+
 -- *** The Devious Delivery ***
 
 -- *** The Forgotten Gift ***
