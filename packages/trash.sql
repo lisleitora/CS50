197fca1766eac768ab7ded9e9e64f34984a92ad9
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-10 12:09:59 +0000

    Tue, Dec 10, 2024, 12:09 PM +00:00

--- packages/trash.sql
+++ packages/trash.sql
@@ -1,6 +1,6 @@
 -- database: packages.db
 SELECT id FROM packages
-JOIN addresses ON (packages.id = package_id)
-JOIN scans ON (addresses.id = address_id)
+JOIN scans ON (packages.id = package_id)
+JOIN s ON (addresses.id = address_id)
 WHERE from_address_id = '900 Somerville Avenue'
 AND to_address_id = '2 Finnegan Street'
