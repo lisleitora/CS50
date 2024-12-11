6763df53200417c4d8aceb42aa07a5c47a0a8ee3
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-11 13:30:34 +0000

    Wed, Dec 11, 2024, 1:30 PM +00:00

--- packages/trash.sql
+++ packages/trash.sql
@@ -2,6 +2,8 @@
 -- SELECT * FROM addresses
 -- WHERE address LIKE '%2%Finnegan%Street%uptown%'
 
- SELECT * FROM packages
-WHERE from_address_id = '432'
+SELECT * FROM packages
+WHERE from_address_id = '900 Somerville Avenue'
 -- 432
+SELECT * FROM packages
+WHERE from_address_id = '432'
