54c64a6fb01dc2895a3a49d124ea3618e8d5f2c7
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-11 13:55:27 +0000

    Wed, Dec 11, 2024, 1:55 PM +00:00

--- packages/trash.sql
+++ packages/trash.sql
@@ -1,1 +1,9 @@
 -- database: packages.db
+SELECT id FROM addresses
+WHERE address LIKE '728 Maple Place'
+OR address LIKE '109 Tileston Street';
+-- result: 4983 and 9873
+
+SELECT * FROM packages
+WHERE from_address_id = ''
+AND to_address_id = '';
