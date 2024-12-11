73742f1e7b8a59d412e92402f009acebd4638822
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-11 13:39:53 +0000

    Wed, Dec 11, 2024, 1:39 PM +00:00

--- packages/trash.sql
+++ packages/trash.sql
@@ -1,10 +1,17 @@
 -- database: packages.db
--- SELECT * FROM addresses
--- WHERE address LIKE '%2%Finnegan%Street%uptown%'
 
---SELECT id FROM addresses
---WHERE address LIKE '900 Somerville Avenue'
+SELECT id FROM addresses
+WHERE address LIKE '900 Somerville Avenue';
 -- result address id: 432
 
-SELECT * FROM packages
+SELECT id FROM packages
 WHERE from_address_id = '432'
+AND contents LIKE '%Congratulatory%';
+-- resutl packge id: 384
+
+SELECT * FROM scans
+WHERE scans.package_id = '384'
+AND action = 'Drop';
+
+SELECT * FROM addresses
+WHERE
