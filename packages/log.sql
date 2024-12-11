55152499ecefceade9c798341ddcd49695484bdd
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-11 15:00:48 +0000

    Wed, Dec 11, 2024, 3:00 PM +00:00

--- packages/log.sql
+++ packages/log.sql
@@ -25,8 +25,8 @@ WHERE id = '854';
 
 -- *** The Devious Delivery ***
 SELECT * FROM packages
-WHERE contents LIKE '%duck%';
--- AND from_address_id IS NULL;
+WHERE contents LIKE '%duck%'
+AND from_address_id IS NULL;
 -- to address id: 50,
 SELECT * FROM addresses
 WHERE id = '50'
