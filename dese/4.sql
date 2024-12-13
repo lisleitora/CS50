1bd8500e418f01c38e267fb1143ca48cc5316a4d
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-13 16:02:35 +0000

    Fri, Dec 13, 2024, 4:02 PM +00:00

--- dese/4.sql
+++ dese/4.sql
@@ -1,5 +1,5 @@
 -- database: dese.db
-SELECT city, COUNT(id) as count FROM schools
+SELECT city, COUNT(schools.id) as count FROM schools
 JOIN districts ON (district_id = districts.id)
 WHERE type NOT LIKE 'Charter%'
 ORDER BY count
