4289cf1d43f1efc9cc371ea849768138995c971a
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-13 11:33:53 +0000

    Fri, Dec 13, 2024, 11:33 AM +00:00

--- dese/1.sql
+++ dese/1.sql
@@ -2,5 +2,5 @@
 SELECT schools.name, schools.city FROM schools
 JOIN districts ON (district_id = districts.id)
 WHERE districts.state LIKE '%MA%'
---AND districts.type NOT LIKE 'Charter School';
+AND districts.type NOT LIKE 'Charter%';
 
