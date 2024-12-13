c10a386fda34746c098559f23200eadfe2b55347
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-13 11:03:56 +0000

    Fri, Dec 13, 2024, 11:03 AM +00:00

--- dese/1.sql
+++ dese/1.sql
@@ -1,5 +1,5 @@
 -- database: dese.db
-SELECT schools.name, city FROM schools
+SELECT schools.name, schools.city FROM schools
 JOIN districts ON (district_id = districts.id)
 WHERE districts.name LIKE '%Massachusetts%'
 AND type NOT LIKE 'Charter School'
