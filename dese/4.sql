6f20db90e063549112e94737734e682d2107de32
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-14 14:30:18 +0000

    Sat, Dec 14, 2024, 2:30 PM +00:00

--- dese/4.sql
+++ dese/4.sql
@@ -2,5 +2,6 @@
 SELECT districts.city, COUNT(schools.id) as count FROM schools
 JOIN districts ON (district_id = districts.id)
 WHERE schools.type NOT LIKE 'Charter%'
-ORDER BY count
+GROUP BY districts.city
+ORDER BY count DESC, districts.city
 LIMIT 10
