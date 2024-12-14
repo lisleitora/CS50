fc7417c15fcd2d910000a9c7f37b1f98ccc60f5f
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-14 14:45:25 +0000

    Sat, Dec 14, 2024, 2:45 PM +00:00

--- dese/4.sql
+++ dese/4.sql
@@ -1,7 +1,7 @@
 -- database: dese.db
 SELECT districts.city, COUNT(schools.id) as count FROM schools
 JOIN districts ON (district_id = districts.id)
-WHERE schools.type LIKE '%Public%'
+WHERE schools.type = 'Public School'
 GROUP BY districts.city
 ORDER BY count DESC, districts.city
 LIMIT 10
--- dese/5.sql
+++ dese/5.sql
@@ -1,3 +1,4 @@
 -- database: dese.db
 SELECT * FROM schools
+WHERE type <> 'Public School'
 LIMIT 30
