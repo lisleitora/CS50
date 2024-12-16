148963973eab8970ede02c234b28280d8084580b
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-16 17:49:13 +0000

    Mon, Dec 16, 2024, 5:49 PM +00:00

--- dese/13.sql
+++ dese/13.sql
@@ -1,2 +1,4 @@
-SELECT schools.name, per_pupil_expenditure FROM schools
+-- database: dese.db
+SELECT schools.name, per_pupil_expenditure FROM schools AS s
 JOIN expenditures ON (schools.district_id = expenditures.district_id)
+WHERE s.city LIKE 'New York%'
