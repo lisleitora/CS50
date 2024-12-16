7be858dc51b72fd19f2a6dc289a2f31f5ba6288b
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-16 17:23:22 +0000

    Mon, Dec 16, 2024, 5:23 PM +00:00

---
+++ dese/11.sql
@@ -0,0 +1,5 @@
+SELECT districts.name, per_pupil_expenditure FROM districts
+JOIN expenditures ON (districts.id = expenditures.district_id)
+WHERE type = 'Public School District'
+GROUP BY districts.id
+ORDER BY per_pupil_expenditure DESC
