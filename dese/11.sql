5bd0b42941625eba9ea53376e6c82bf6e7cb0f4c
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-16 17:27:56 +0000

    Mon, Dec 16, 2024, 5:27 PM +00:00

--- dese/11.sql
+++ dese/11.sql
@@ -1,4 +1,5 @@
 -- database: dese.db
 SELECT schools.name, per_pupil_expenditure FROM schools
 JOIN expenditures ON (schools.district_id = expenditures.district_id)
+JOIN graduation_rates
 ORDER BY per_pupil_expenditure DESC, schools.name
