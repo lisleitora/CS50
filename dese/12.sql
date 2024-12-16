20538e14f9f7bb2339c88701d4fd0c5bfdf477c6
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-16 18:04:27 +0000

    Mon, Dec 16, 2024, 6:04 PM +00:00

--- dese/12.sql
+++ dese/12.sql
@@ -3,4 +3,9 @@ SELECT * FROM districts AS d
 JOIN expenditures AS e ON (e.district_id = d.id)
 JOIN staff_evaluations AS se ON (se.district_id = d.id)
 WHERE d.type = 'Public School District'
-AND
+AND se.exemplary > (
+ SELECT AVG(exemplary)
+ FROM staff_evaluations)
+ AND e.per_pupil_expenditure > (
+ SELECT AVG(per_pupil_expenditure)
+ FROM expenditures)
