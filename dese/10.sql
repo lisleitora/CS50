f2c786c52834f5a77945264e9e8e0da759fbf06b
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-16 17:20:54 +0000

    Mon, Dec 16, 2024, 5:20 PM +00:00

--- dese/10.sql
+++ dese/10.sql
@@ -1,7 +1,7 @@
 -- database: dese.db
 SELECT districts.name, per_pupil_expenditure FROM districts
 JOIN expenditures ON (districts.id = expenditures.district_id)
-WHERE type = 'Public School'
+WHERE type = 'Public School District'
 GROUP BY districts.id
 ORDER BY per_pupil_expenditure DESC
 LIMIT 10
