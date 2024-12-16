76a41b127dc375b778229843cac28f63e72f3dd9
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-16 17:18:19 +0000

    Mon, Dec 16, 2024, 5:18 PM +00:00

--- dese/10.sql
+++ dese/10.sql
@@ -1,4 +1,6 @@
-SELECT districts.name, pupils FROM districts
+-- database: dese.db
+SELECT districts.name, per_pupil_expenditure FROM districts
 JOIN expenditures ON (districts.id = expenditures.district_id)
 GROUP BY districts.id
-ORDER BY pupils DESC
+ORDER BY per_pupil_expenditure DESC
+LIMIT 10
