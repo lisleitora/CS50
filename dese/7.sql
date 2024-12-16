c5207dcb4808c8e2ede3c8ff654d699f9d6fe640
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-16 14:35:53 +0000

    Mon, Dec 16, 2024, 2:35 PM +00:00

--- dese/7.sql
+++ dese/7.sql
@@ -1,5 +1,4 @@
 -- database: dese.db
 SELECT schools.name FROM schools
 JOIN districts ON (district_id = districts.id)
-WHERE schools.city LIKE '%cambridge%'
-AND districts.name LIKE '%cambridge%'
+WHERE districts.name LIKE '%cambridge%'
