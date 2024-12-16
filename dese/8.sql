333680cd0410948c7227b10cbbdca41443e170fe
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-16 17:08:03 +0000

    Mon, Dec 16, 2024, 5:08 PM +00:00

--- dese/8.sql
+++ dese/8.sql
@@ -3,3 +3,4 @@ SELECT schools.name, pupils FROM schools
 JOIN expenditures ON (schools.district_id = expenditures.district_id)
 JOIN districts ON (districts.id = schools.district_id)
 GROUP BY districts.id
+ORDER BY 
