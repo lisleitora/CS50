92797888ef8576c211eb3aab1ee82c6336450315
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-17 20:46:04 +0000

    Tue, Dec 17, 2024, 8:46 PM +00:00

--- moneyball/9.sql
+++ moneyball/9.sql
@@ -2,7 +2,7 @@
 SELECT t.name, ROUND(AVG(s.salary), 2) AS "average salary"
 FROM teams AS t
 JOIN salaries AS s ON (s.team_id = t.id)
-WHERE t.year = '2001'
+WHERE s.year = '2001'
 GROUP BY t.name
 ORDER BY "average salary"
 LIMIT 5
