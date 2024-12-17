4fe0e4e8b1215feeee4630185b2a7c0b12e42ec0
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-17 18:13:44 +0000

    Tue, Dec 17, 2024, 6:13 PM +00:00

--- moneyball/6.sql
+++ moneyball/6.sql
@@ -2,5 +2,6 @@
 SELECT t.name, SUM(p.H + p.`2B` + p.`3B`) AS total_hits FROM teams AS t
 JOIN performances AS p ON (t.id = p.team_id)
 WHERE p.year = '2001'
+GROUP BY t.name
 ORDER BY total_hits DESC
 LIMIT 5
