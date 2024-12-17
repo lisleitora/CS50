e9f3bb8e329c5cf8458bf2fc847a2f1b6fb5529b
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-17 20:38:29 +0000

    Tue, Dec 17, 2024, 8:38 PM +00:00

--- moneyball/9.sql
+++ moneyball/9.sql
@@ -1,5 +1,6 @@
 -- database: moneyball.db
-SELECT t.name, ROUND(AVG(s.salary), 2) AS average_salary FROM teams AS t
+SELECT t.name, ROUND(AVG(s.salary), 2) AS average_salary
+FROM teams AS t
 JOIN salaries AS s ON (s.team_id = t.id)
 GROUP BY t.name
 ORDER BY average_salary
