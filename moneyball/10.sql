1e5b6ecbc4cb326cbb016e65673b26d5ef502f56
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-17 21:35:15 +0000

    Tue, Dec 17, 2024, 9:35 PM +00:00

--- moneyball/10.sql
+++ moneyball/10.sql
@@ -5,4 +5,4 @@ SELECT pl.first_name,
  pe.HR, s.year FROM players AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
 JOIN performances AS pe ON (pe.player_id = pl.id)
-ORDER BY pl.id, s.year DESC, 
+ORDER BY pl.id, s.year DESC, pe.HR DESC, s.salary DESC
