9a0682cd37a855a807955c16a7f348758ab2a250
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-17 16:00:22 +0000

    Tue, Dec 17, 2024, 4:00 PM +00:00

--- moneyball/4.sql
+++ moneyball/4.sql
@@ -1,5 +1,6 @@
 -- database: moneyball.db
 SELECT p.first_name, p.last_name, salary FROM players AS p
 JOIN salaries AS s ON (s.player_id = p.id)
+WHERE s.year = '2001'
 ORDER BY salary, p.first_name, p.last_name, p.id
 LIMIT 50
