a8f329ccba9b04dea08e0366d8ccb30df331e922
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-17 20:32:26 +0000

    Tue, Dec 17, 2024, 8:32 PM +00:00

--- moneyball/9.sql
+++ moneyball/9.sql
@@ -1,5 +1,6 @@
 -- database: moneyball.db
-SELECT t.name, s.salary FROM teams AS t
+SELECT t.name, AVG(s.salary)  FROM teams AS t
 JOIN salaries AS s ON (s.team_id = t.id)
+GROUP BY t.name
 ORDER BY salary
 LIMIT 5
