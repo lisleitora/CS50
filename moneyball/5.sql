fe008f961c5ecc4ce31f05c3f4482f6fac40c7c9
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-17 16:16:40 +0000

    Tue, Dec 17, 2024, 4:16 PM +00:00

--- moneyball/5.sql
+++ moneyball/5.sql
@@ -1,2 +1,7 @@
-SELECT t.name FROM team AS t
-JOIN performances AS pe ON ()
+-- database: moneyball.db
+SELECT t.name FROM teams AS t
+JOIN performances AS pe ON (t.id = pe.team_id)
+JOIN players AS pl ON (pl.id = pe.player_id)
+WHERE pl.first_name = 'Satchel'
+AND pl.last_name = 'Paige'
+GROUP BY t.name
