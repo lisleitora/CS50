c48501b58d68ea7f3e8635b45879588185bdef7c
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-17 20:52:54 +0000

    Tue, Dec 17, 2024, 8:52 PM +00:00

--- moneyball/8.sql
+++ moneyball/8.sql
@@ -1,6 +1,6 @@
 -- database: moneyball.db
 SELECT salary FROM salaries AS s
 JOIN performances AS p ON (p.player_id = s.player_id)
-WHERE p.year = 2001
+WHERE p.year = 2001 AND s.year = 2001
 ORDER BY p.HR DESC
 LIMIT 1
