57c696b2606a87f7addf22ab697f2847bb2d09d5
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-17 15:10:39 +0000

    Tue, Dec 17, 2024, 3:10 PM +00:00

--- moneyball/3.sql
+++ moneyball/3.sql
@@ -2,6 +2,6 @@
 SELECT pe.year, pe.HR FROM performances AS pe
 JOIN players AS pl ON (pl.id = pe.player_id)
 WHERE pl.first_name = 'Ken'
-AND pl.last_name = 'Griffin'
+AND pl.last_name = 'Griffey'
 AND pl.birth_year = '1969'
 ORDER BY pe.year DESC
