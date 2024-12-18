172341fa9175ffad2ae98529a250b8b4e2907b1f
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-18 13:28:27 +0000

    Wed, Dec 18, 2024, 1:28 PM +00:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -1,8 +1,10 @@
 -- database: moneyball.db
-SELECT pl.first_name, pl.last_name FROM players AS pl
+SELECT pl.first_name, pl.last_name, s.salary/pe.RBI AS dollars_per_hit
+FROM players AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
 JOIN performances AS pe ON (pe.player_id = pl.id
-AND pe.year = s.year)WHERE pe.year = '2001'
+AND pe.year = s.year)
+WHERE pe.year = '2001'
 AND pe.H NOT LIKE '0'
-ORDER BY s.salary/pe.H AS dollars_per_hit, pl.id
+ORDER BY dollars_per_hit, pl.first_name, pl.last_name
 LIMIT 10
