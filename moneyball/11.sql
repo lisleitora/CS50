5c4bedbd067c8ec9e1c26809a93ae1647fc4c26d
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-18 12:17:28 +0000

    Wed, Dec 18, 2024, 12:17 PM +00:00

--- moneyball/11.sql
+++ moneyball/11.sql
@@ -1,4 +1,4 @@
 SELECT pl.first_name, pl.last_name, s.salary/pe.H AS dollars_per_hit
 FROM player AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
-JOIN performances AS pe ON ()
+JOIN performances AS pe ON (pe.player_id = pl.id)
