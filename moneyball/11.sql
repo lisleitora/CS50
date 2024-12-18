c69ccfeb853869c11a719d0d0554b4a50fd52b8e
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-18 12:28:10 +0000

    Wed, Dec 18, 2024, 12:28 PM +00:00

--- moneyball/11.sql
+++ moneyball/11.sql
@@ -3,4 +3,4 @@ FROM player AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
 JOIN performances AS pe ON (pe.player_id = pl.id)
 --you false bitch motherfuckin dog go back to hell you fucking viper, you traitor, you damn dirty criminal, how dare you to jump off me 5 seconds after you came, you just wanted me for the cookies huh? you look at me with those (literal) puppy eyes... fuckin cute dog imma give ya more food. you greedy bastard. why you must be so cute?
--- she ate my food. when i had no more, she left. its always like this. she does bad things, pees in the carpet, but then, THEN she looks at me with those eyes and i absolutely cant do anything because of her cuteness. 
+-- she ate my food. when i had no more, she left. its always like this. she does bad things, pees in the carpet, but then, THEN she looks at me with those eyes and i absolutely cant do anything because of her cuteness. one day i will overcome my primal instincts to praise babies cats and dogs and ignoring your overwhelming cuteness, i will make justice.
