d4fe76a28e43b49dbf31beae16cdea72c6c2337b
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-18 13:29:54 +0000

    Wed, Dec 18, 2024, 1:29 PM +00:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -5,6 +5,6 @@ JOIN salaries AS s ON (s.player_id = pl.id)
 JOIN performances AS pe ON (pe.player_id = pl.id
 AND pe.year = s.year)
 WHERE pe.year = '2001'
-AND pe.H NOT LIKE '0'
+AND pe.RBI NOT LIKE '0'
 ORDER BY dollars_per_hit, pl.first_name, pl.last_name
 LIMIT 10
