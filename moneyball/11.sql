12c1901cf58e9a325fc103faf50c40a494eac6fd
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-18 12:22:33 +0000

    Wed, Dec 18, 2024, 12:22 PM +00:00

--- moneyball/11.sql
+++ moneyball/11.sql
@@ -2,3 +2,5 @@ SELECT pl.first_name, pl.last_name, s.salary/pe.H AS dollars_per_hit
 FROM player AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
 JOIN performances AS pe ON (pe.player_id = pl.id)
+--you false bitch motherfuckin dog go back to hell you fucking viper, you traitor, you damn dirty criminal, how dare you to jump off me 5 seconds after you came, you just wanted me for the cookies huh? you look at me with those (literal) puppy eyes... fuckin cute dog imma give ya more food. you greedy bastard. why you must be so cute?
+-- 
