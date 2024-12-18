ac0af92266a32498d7adb61ea43c2998c1bc6c48
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-18 13:44:13 +0000

    Wed, Dec 18, 2024, 1:44 PM +00:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -2,7 +2,8 @@
 SELECT pl.first_name, pl.last_name FROM players AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
 JOIN performances AS pe ON (pe.player_id = pl.id)
-WHERE pl.id IN ((SELECT pl.id
+WHERE pl.id IN
+ ((SELECT pl.id
 FROM players AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
 JOIN performances AS pe ON (pe.player_id = pl.id
@@ -11,7 +12,9 @@ WHERE pe.year = '2001'
 AND pe.RBI NOT LIKE '0'
 ORDER BY s.salary/pe.RBI, pl.first_name, pl.last_name
 LIMIT 10))
- AND pl.id IN ((SELECT pl.id
+
+ AND pl.id IN
+ ((SELECT pl.id
 FROM players AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
 JOIN performances AS pe ON (pe.player_id = pl.id
