5e4e83b2299de2c8ea4992f4bae9bcf3eead15ff
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-19 11:27:09 +0000

    Thu, Dec 19, 2024, 11:27 AM +00:00

--- moneyball/10.sql
+++ moneyball/10.sql
@@ -1,9 +1,15 @@
 -- database: moneyball.db
-SELECT pl.first_name,
- pl.last_name,
- s.salary,
- pe.HR, s.year FROM players AS pl
+SELECT pl.first_name
+  , pl.last_name
+  , s.salary
+  , pe.HR
+  , s.year
+FROM players AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
-JOIN performances AS pe ON (pe.player_id = pl.id
-AND s.year = pe.year)
-ORDER BY pl.id, s.year DESC, pe.HR DESC, s.salary DESC
+JOIN performances AS pe ON (
+    pe.player_id = pl.id
+    AND s.year = pe.year
+  )
+ORDER BY pl.id, s.year DESC
+  , pe.HR DESC
+  , s.salary DESC
--- moneyball/11.sql
+++ moneyball/11.sql
@@ -1,13 +1,16 @@
 -- database: moneyball.db
-SELECT pl.first_name, pl.last_name, s.salary/pe.H AS dollars_per_hit
+SELECT pl.first_name
+  , pl.last_name
+  , s.salary / pe.H AS dollars_per_hit
 FROM players AS pl
 JOIN salaries AS s ON (s.player_id = pl.id)
-JOIN performances AS pe ON (pe.player_id = pl.id
-AND pe.year = s.year)
--- how dare you to jump off me 5 seconds after you came, you just wanted me for the cookies huh? you look at me with those (literal) puppy eyes... cute dog imma give ya more food. you greedy pup. why you must be so cute?
--- she ate my food. when i had no more, she left. its always like this. she does bad things, pees in the carpet, but then, THEN she looks at me with those eyes and i absolutely cant do anything because of her cuteness. one day i will overcome my primal instincts to praise babies cats and dogs and ignoring your overwhelming cuteness, i will make justice.
--- one day, no treats will be served. (how does my mom resist you? is she not human?)
+JOIN performances AS pe ON (
+    pe.player_id = pl.id
+    AND pe.year = s.year
+  ) -- how dare you to jump off me 5 seconds after you came, you just wanted me for the cookies huh? you look at me with those (literal) puppy eyes... cute dog imma give ya more food. you greedy pup. why you must be so cute?
+  -- she ate my food. when i had no more, she left. its always like this. she does bad things, pees in the carpet, but then, THEN she looks at me with those eyes and i absolutely cant do anything because of her cuteness. one day i will overcome my primal instincts to praise babies cats and dogs and ignoring your overwhelming cuteness, i will make justice.
+  -- one day, no treats will be served. (how does my mom resist you? is she not human?)
 WHERE pe.year = '2001'
-AND pe.H NOT LIKE '0'
+  AND pe.H NOT LIKE '0'
 ORDER BY dollars_per_hit, pl.first_name, pl.last_name
 LIMIT 10
