8184b132f170c5b4ba51fd15c9119c3a2a3de537
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-12-20 13:33:33 +0000

    Fri, Dec 20, 2024, 1:33 PM +00:00

--- atl/schema.sql
+++ atl/schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE "passengers"(
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
-    "age" NOT NULL INTEGER
+    "age" INTEGER NOT NULL
 );
 
 CREATE TABLE "check_in"(
     "datetime" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "flight" INTEGER NOT NULL
+    "flight" INTEGER NOT NULL,
     FOREIGN KEY("flight") REFERENCES "flights"("flight_number")
 );
 
 CREATE TABLE "airlines"(
     "name" TEXT NOT NULL,
-    "concourse" TEXT NOT NULL CHECK("concourse" IN ('A', 'B', 'C', 'D', 'E', 'F', 'T'))
+    "concourse" TEXT NOT NULL CHECK("concourse" IN ('A', 'B', 'C', 'D', 'E', 'F', 'T')),
     PRIMARY KEY("name")
 );
 
