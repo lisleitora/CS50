02949b411a0a414c8a9bfe80ee19d2145414dad7
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2025-01-09 12:39:58 +0000

    Thu, Jan 9, 2025, 12:39 PM +00:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -1,7 +1,11 @@
-CREATE TABLE "user"(
+CREATE TABLE "users"(
  "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 PRIMARY KEY("username")
+);
+
+CREATE TABLE "schools"(
+    
 );
