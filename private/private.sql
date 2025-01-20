a1a2a7198808c5bf73cc38ac1a9c6983ca69dd7f
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2025-01-20 21:02:00 +0000

    Mon, Jan 20, 2025, 9:02 PM +00:00

--- private/private.sql
+++ private/private.sql
@@ -1,5 +1,11 @@
 -- database: private.db
-
+CREATE TABLE "users"(
+ "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+PRIMARY KEY("username")
+);
 
 
 CREATE VIEW "message" AS
