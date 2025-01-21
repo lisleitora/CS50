8601c6c173f1392a3241eb389476f389136a0218
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2025-01-21 16:09:55 +0000

    sherloc homes

--- private/private.sql
+++ private/private.sql
@@ -1,20 +1,34 @@
 -- database: private.db
 CREATE TABLE "cleaned_sentences"(
- "id" INTEGER NOT NULL,
-    "sentence" TEXT NOT NULL,
-    "first_character" INTEGER NOT NULL,
-    "length" INTEGER NOT NULL,
-PRIMARY KEY("id")
+ "id" INTEGER,
+    "sentence" TEXT,
+    "first_character" INTEGER,
+    "length" INTEGER,
+    PRIMARY KEY(id)
 );
 
 INSERT INTO cleaned_sentences (id, sentence)
-SELECT
-    CASE
-        WHERE id IN(),
-FROM sentences;
+SELECT sentences.id, sentences.sentence
+FROM sentences
+WHERE id IN (14, 114, 618, 630, 932, 2230, 2346, 3041);
+
+    REPLACE INTO cleaned_sentences (id, first_character, length)
+VALUES
+    (14, 98, 4),
+    (114, 3, 5),
+    (618, 72, 9),
+    (630, 7, 3),
+    (932, 12, 5),
+    (2230, 50, 7),
+    (2346, 44, 10),
+    (3041, 14, 5)
+    ON CONFLICT (id)
+DO UPDATE SET
+    first_character = excluded.first_character,
+    length = excluded.length;
 
 CREATE VIEW "message" AS
-SELECT substr("sentence", "character", "length") AS "phrase"
+SELECT substr(sentence, first_character, length) AS "phrase"
  FROM cleaned_sentences;
 
 SELECT "phrase" FROM "message";
