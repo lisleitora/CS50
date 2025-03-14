d34e577b8e64d8f033c0585b3e10a0dd41d83fe2
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2025-03-14 11:38:10 +0000

    Fri, Mar 14, 2025, 11:38 AM +00:00

--- tip/tip.py
+++ tip/tip.py
@@ -8,12 +8,13 @@ def main():
 def dollars_to_float(d):
     # TODO
     d.removeprefix('$')
-    return d.float()
+    return float(d)
 
 
 def percent_to_float(p):
     # TODO
     p.removesuffix('%')
+    return float(p)
 
 main()
 print(percent_to_float + dollars_to_float)
