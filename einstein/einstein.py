9e1bbda090f5afce5bc96dc0717e6b79dd3e2dbc
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2025-03-14 10:14:12 +0000

    Fri, Mar 14, 2025, 10:14 AM +00:00

---
+++ einstein/einstein.py
@@ -0,0 +1,5 @@
+# Asking for the mass so i can convert it
+x = input("A mass to energy translating device! Try it now: ")
+
+# Transforming kiograms(mass) to joules and showing it on the terminal
+print(int(x)*90000000000000000)
